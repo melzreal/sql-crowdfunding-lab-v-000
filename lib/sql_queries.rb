1834f7b5b6d6cb277a8a4294d37a69b077fca67c
Melissa Real <melissa.real@hotmail.com>
2018-09-30 11:58:52 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, SUM(projects.funding_goal), SUM(pledges.amount) FROM projects
 JOIN pledges ON projects.id = pledges.project_id
 GROUP BY pledges.project_id
-WHERE SUM(pledges.amount) > SUM(projects.funding_goal);"
+HAVING SUM(pledges.amount) > SUM(projects.funding_goal);"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
