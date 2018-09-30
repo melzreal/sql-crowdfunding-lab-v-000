82708fd0c30feac7929d2a355c413283a2552db1
Melissa Real <melissa.real@hotmail.com>
2018-09-30 12:42:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,14 +7,16 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
   def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-  "SELECT projects.title, SUM(pledges.amount) FROM projects
+  "SELECT projects.title, SUM(pledges.amount) 
+  FROM projects
   JOIN pledges ON projects.id = pledges.project_id
   GROUP BY pledges.project_id
   ORDER BY projects.title ASC;"
   end
 
   def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, SUM(pledges.amount) FROM users
+  "SELECT users.name, users.age, SUM(pledges.amount) 
+  FROM users
   JOIN pledges ON users.id = pledges.user_id
   GROUP BY pledges.user_id
   ORDER BY users.name ASC;"
@@ -29,14 +31,18 @@
   end
 
   def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-    "SELECT users.name, SUM(pledges.amount) FROM users
+    "SELECT users.name, SUM(pledges.amount) 
+    FROM users
     JOIN pledges ON users.id = pledges.user_id
     GROUP BY users.name
     ORDER BY SUM(pledges.amount);"
   end
 
   def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT projects.category, pledges.amount 
+  FROM projects
+  JOIN pledges on projects.id = pledges.project_id
+  WHERE category = 'music'"
   end
 
   def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
